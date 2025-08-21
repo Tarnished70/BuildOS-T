c2e197ce722c8cbe9c9e2e056d2c8aae3d67cd7f
wHo-EM-i <ehteshammalik18998@gmail.com>
2025-08-22 00:39:56 +0530

    CI: test
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -6,4 +6,4 @@
 tg "SGSI Build Started!
 Status: $progress"
 
-#sudo bash LinkToGSI.sh OEM_URL RomType
+sudo bash LinkToGSI.sh https://dl.google.com/dl/android/aosp/lynx-bp1a.250505.005.b1-factory-45a1393f.zip Pixel
