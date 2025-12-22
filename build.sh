ac4e6811cb3764fde1b4c29a39277140136b6818
wHo-EM-i <ehteshammalik18998@gmail.com>
2025-12-23 01:06:05 +0530

    ci: crdroid11 Hotfix | R
    Signed-off-by: wHo-EM-i <ehteshammalik18998@gmail.com>

--- build.sh
+++ build.sh
@@ -3,12 +3,19 @@ login_main
 . build/envsetup.sh
 # RBE
 . /tmp/ci/rbe
-#export NINJA_REMOTE_NUM_JOBS=150
-export RBE_CXX_LINKS_EXEC_STRATEGY=local
-#export RBE_METALAVA_EXEC_STRATEGY=local
-#export RBE_LOG_LEVEL=debug
-#export USE_CCACHE=0
+# --- BuildBuddy Connection Settings ---
+#export RBE_service="nexgang.buildbuddy.io:443"
+#export RBE_remote_headers="x-buildbuddy-api-key=0mjw9c0kEWHrzTHaAMTy"
+#export RBE_remote_executor="grpcs://nexgang.buildbuddy.io"
+#export RBE_remote_cache="grpcs://nexgang.buildbuddy.io"
+#export RBE_remote_timeout="10m"
+
+#export RBE_CXX_LINKS_EXEC_STRATEGY=local
+export RBE_LOG_LEVEL=debug
+export USE_CCACHE=0
+export NINJA_ARGS="-j16"
 env | grep RBE
+
 lunch lineage_lavender-userdebug
 
 build_gapps=0
@@ -18,7 +25,7 @@ export TZ=Asia/Kolkata
 export SELINUX_IGNORE_NEVERALLOWS=true
 export RELAX_USES_LIBRARY_CHECK=true
 export WITH_GMS=false
-#export USE_CCACHE=0
+export USE_CCACHE=0
 
 exp_gapps () {
 export USE_GAPPS=false
@@ -26,7 +33,7 @@ export USE_GAPPS=false
 
 compile_plox () {
 
-m bacon -j16
+m bacon -j80
 
 # KSU bc -_-
 if [ -e "/tmp/rom/out/error.log" ] && [ ! -e out/target/product/*/*.zip ] && [ $(cat /tmp/rom/out/error.log | grep -o -e 'KernelSU' -e 'FAILED: ' | head -n 1) ]; then
